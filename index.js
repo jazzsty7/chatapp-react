e9a89df497ef9acbc4a55581701c0e7d36663ba6
jazzsty7 <jazzsty@gmail.com>
2026-07-04 00:27:03 +0900

    커밋 문구 수정

--- index.js
+++
@@ -1,6 +0,0 @@
-const { createServer } = require("http");
-const app = require("./app");
-const {Server} = require("socket.io");
-
-const httpServer = createServer(app);
-const io = new Server(httpServer);
